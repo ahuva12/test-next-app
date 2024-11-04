2206f20dae14af018bb173e4f9a3b3b29a698a0a
ahuva12 <ahuvaeshit@gmail.com>
2024-11-04 12:49:25 +0200

    nk

--- src/service/httpBooks.ts
+++ src/service/httpBooks.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const http = axios.create({
-    baseURL: "http://localhost:3000/api/books",
+    baseURL: "/api/books",
     headers: {
         "Content-type": "application/json",
         "Access-Control-Allow-Origin": "*"
