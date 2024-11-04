752dff68f91f920a49cf50672246adf4cb121c71
ahuva12 <ahuvaeshit@gmail.com>
2024-11-04 11:34:33 +0200

    cors

--- src/service/httpBooks.ts
+++ src/service/httpBooks.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 export const http = axios.create({
     baseURL: "http://localhost:3000/api/books",
     headers: {
-        "Content-type": "application/json"
+        "Content-type": "application/json",
+        "Access-Control-Allow-Origin": "*"
     }
 });
