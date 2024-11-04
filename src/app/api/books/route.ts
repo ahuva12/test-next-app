a779314a3550031704e7b12b272c313d69da0461
ahuva12 <ahuvaeshit@gmail.com>
2024-11-04 12:42:50 +0200

    cors

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -1,8 +1,20 @@
 import { NextResponse } from 'next/server';
 import { Gbooks } from './db';
 
+// export async function GET(request: Request) {
+//     return NextResponse.json(Gbooks, {
+//         status: 200,
+//     });
+// }
+
+
 export async function GET(request: Request) {
-    return NextResponse.json(Gbooks, {
+    return new Response(JSON.stringify(Gbooks), {
         status: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        },
     });
 }
