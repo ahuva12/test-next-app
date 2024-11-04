0926e5edbf6ed6990188fa4834db25be03ec44d7
ahuva12 <ahuvaeshit@gmail.com>
2024-11-04 12:03:00 +0200

    ss

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -2,42 +2,7 @@ import { NextResponse } from 'next/server';
 import { Gbooks } from './db';
 
 export async function GET(request: Request) {
-    const origin = request.headers.get('origin');
-    const allowedOrigins = ['https://test-next-app-roan.vercel.app', 'http://localhost:3000'];
-
-    // Create a response with CORS headers
-    const response = NextResponse.json(Gbooks, {
+    return NextResponse.json(Gbooks, {
         status: 200,
     });
-
-    // Check if the origin is allowed, and set CORS headers if so
-    if (origin && allowedOrigins.includes(origin)) {
-        response.headers.set('Access-Control-Allow-Origin', origin);
-        response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-        response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    }
-
-    return response;
 }
-
-export async function OPTIONS() {
-    const headers = new Headers();
-    headers.set('Access-Control-Allow-Origin', '*');
-    headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-    return new Response(null, {
-        status: 204,
-        headers,
-    });
-}
-
-
-// import { NextResponse } from 'next/server';
-// import { Gbooks } from './db';
-
-// export async function GET(request: Request) {
-//     return NextResponse.json(Gbooks, {
-//         status: 200,
-//     });
-// }
--- src/service/httpBooks.ts
+++ src/service/httpBooks.ts
@@ -4,6 +4,6 @@ export const http = axios.create({
     baseURL: "http://localhost:3000/api/books",
     headers: {
         "Content-type": "application/json",
-        "Access-Control-Allow-Origin": "*"
+        "access-controlallow-origin": "*"
     }
 });
